39c9901f709aeac81e455efd9b48e860482f2fe5
Stuart Robertson <stuart@delta.co.uk>
2024-09-10 12:05:11 +0100

    Initial Dev Complete

--- package/bin/get_nodes_helper.py
+++ package/bin/get_nodes_helper.py
@@ -1,9 +1,11 @@
 import json
 import logging
+import requests
 
 import import_declare_test
 from solnlib import conf_manager, log
 from splunklib import modularinput as smi
+from pyhiveapi import Hive, SMS_REQUIRED
 
 
 ADDON_NAME = "idelta_addon_for_hivehome"
@@ -13,27 +15,43 @@ def logger_for_input(input_name: str) -> logging.Logger:
     return log.Logs().get_logger(f"{ADDON_NAME.lower()}_{input_name}")
 
 
-def get_account_api_key(session_key: str, account_name: str):
+def get_device_credentials(session_key: str, account_name: str):
     cfm = conf_manager.ConfManager(
         session_key,
         ADDON_NAME,
         realm=f"__REST_CREDENTIAL__#{ADDON_NAME}#configs/conf-idelta_addon_for_hivehome_account",
     )
     account_conf_file = cfm.get_conf("idelta_addon_for_hivehome_account")
-    return account_conf_file.get(account_name).get("api_key")
+    return account_conf_file.get(account_name).get("hive_username"),account_conf_file.get(account_name).get("hive_password"), account_conf_file.get(account_name).get("device_key"), account_conf_file.get(account_name).get("device_password")
+
+def device_auth(logger: logging.Logger, hive_username: str,hive_password: str,device_group_key: str,device_key: str,device_password: str):
+    logger.info("Starting Hive Device Authentication")
+    session = Hive(
+        username=hive_username,
+        password=hive_password)
+    session.auth.device_group_key=device_group_key
+    session.auth.device_key=device_key
+    session.auth.device_password=device_password
+    logger.info("Running deviceLogin()")
+    session.deviceLogin()
+    return session.tokens
 
 
-def get_data_from_api(logger: logging.Logger, api_key: str):
-    logger.info("Getting data from an external API")
-    dummy_data = [
-        {
-            "line1": "hello",
-        },
-        {
-            "line2": "world",
-        },
-    ]
-    return dummy_data
+def get_data_from_api(logger: logging.Logger, session_token: str):
+
+
+    logger.info("Getting data from an Hive API nodes endpoint")
+    url = "https://api.prod.bgchprod.info:443/omnia/nodes"
+    payload = {}
+    headers = {
+        'Content-Type': 'application/vnd.alertme.zoo-6.1+json',
+        'Accept': 'application/vnd.alertme.zoo-6.1+json',
+        'X-Omnia-Client': 'Hive Web Dashboard',
+        'X-Omnia-Access-Token': session_token
+        }
+    response = requests.request("GET", url, headers=headers, data=payload)
+    response_json = json.loads(response.text)
+    return response_json
 
 
 def validate_input(definition: smi.ValidationDefinition):
@@ -65,9 +83,13 @@ def stream_events(inputs: smi.InputDefinition, event_writer: smi.EventWriter):
             )
             logger.setLevel(log_level)
             log.modular_input_start(logger, normalized_input_name)
-            api_key = get_account_api_key(session_key, input_item.get("account"))
-            data = get_data_from_api(logger, api_key)
-            sourcetype = "dummy-data"
+            #get the data required for authentication:
+            hive_username,hive_password,device_group_key,device_key,device_password = get_device_credentials(session_key, input_item.get("account"))
+            #perform authentication and get the session tokens
+            session_tokens = device_auth(hive_username,hive_password,device_group_key,device_key,device_password)
+            #call the API endpoint to get the nodes data
+            data = get_data_from_api(logger, session_tokens["tokenData"]["token"]) 
+            sourcetype = "hive:nodes"
             for line in data:
                 event_writer.write_event(
                     smi.Event(
